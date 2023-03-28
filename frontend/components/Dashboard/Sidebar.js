448b8c8b4dbe81fe3698ef119db50cb91694ae24
Mis311 <55878506+Mis311@users.noreply.github.com>
2023-03-28 22:12:31 +0900

    add dashboard

---
+++ frontend/components/Dashboard/Sidebar.js
@@ -0,0 +1,14 @@
+
+const Sidebar = () => {
+  return (
+    <nav className="column is-2 is-narrow-mobile is-fullheight section is-hidden-mobile">
+      <ul className="menu-list">
+        <li><a>Dashboard</a></li>
+        <li><a>Profile</a></li>
+        <li><a>Settings</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Sidebar;
--- frontend/components/Dashboard/index.jsx
+++ frontend/components/Dashboard/index.jsx
@@ -1,8 +1,42 @@
-const Dashboard = () => (
-  <>
-    <h2 className="title is-2">ArtiFusion</h2>
-    <p>Ley san, I want you to develop this frontend.</p>
-  </>
-);
+import Sidebar from './Sidebar';
+import ContentItem from './ContentItem';
+
+const Dashboard = () => {
+  const contentItems = [
+    {
+      id: 1,
+      title: 'Art 1',
+      username: 'User 1',
+      thumbnailUrl: '/thumbnails/art1.jpg',
+    },
+    {
+      id: 2,
+      title: 'Manga 1',
+      username: 'User 2',
+      thumbnailUrl: '/thumbnails/manga1.jpg',
+    },
+  ];
+
+  return (
+    <div className="columns">
+      <Sidebar />
+      <main className="column">
+        <section className="section">
+          <div className="columns is-multiline">
+            {contentItems.map((item) => (
+              <div className="column is-one-quarter" key={item.id}>
+                <ContentItem
+                  title={item.title}
+                  username={item.username}
+                  thumbnailUrl={item.thumbnailUrl}
+                />
+              </div>
+            ))}
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+};
 
 export default Dashboard;
